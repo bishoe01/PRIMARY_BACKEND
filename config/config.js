dc6fa0d94c94a60de963f55f2b97ad010288959e
ChangSun Park <changsun19991212@gmail.com>
2021-11-03 00:06:04 +0900

    dotenv & ec2 connection

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "primarykey",
+    password: process.env.DATABASE_PASSWORD,
+    database: "PrimaryKey",
+    host: "primarykey.cqok5mvhto8h.ap-northeast-2.rds.amazonaws.com",
+    port: 3306,
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-import express from "express";
+const express = require("express");
+const sequelize = require("./models").sequelize;
+
+const PORT = "3000";
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello primary");
+});
+
+sequelize.authenticate();
+
+app.listen(PORT, () => {
+  console.log(`listening at ${PORT}`);
 });
