46c3e92453b798d8c1b54efc11ec063d6a8f8863
ChangSun Park <changsun19991212@gmail.com>
2021-11-18 21:15:09 +0900

    hotfix : cors middleware

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const Sequelize = require("sequelize");
+const cors = require("cors");
 
 const attendant = require("./routes/attendant");
 const schedule = require("./routes/schedule");
 
-const user = require('./routes/user/user');
-const movie = require('./routes/user/movie');
-const cast  = require('./routes/user/cast');
-const theater  = require('./routes/user/theater');
+const user = require("./routes/user/user");
+const movie = require("./routes/user/movie");
+const cast = require("./routes/user/cast");
+const theater = require("./routes/user/theater");
 
-const ServiceManagerRouter = require('./routes/ServiceManager');
-const SuggestManager = require('./routes/SuggestManager');
-const MenuviewsRouter = require('./routes/Menuview');
-const CheckManagerRouter = require('./routes/CheckManager');
-const ScheduleRouter = require('./routes/ScheduleManager');
+const ServiceManagerRouter = require("./routes/ServiceManager");
+const SuggestManager = require("./routes/SuggestManager");
+const MenuviewsRouter = require("./routes/Menuview");
+const CheckManagerRouter = require("./routes/CheckManager");
+const ScheduleRouter = require("./routes/ScheduleManager");
 
 const PORT = "3000";
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", user);
 app.use("/movies", movie);
@@ -27,11 +29,11 @@ app.use("/theater", theater);
 app.use("/attendant", attendant);
 app.use("/schedule", schedule);
 
-app.use('/service', ServiceManagerRouter);
-app.use('/suggestion',SuggestManager);
-app.use('/menu', MenuviewsRouter);
-app.use('/check', CheckManagerRouter);
-app.use('/employee', ScheduleRouter);
+app.use("/service", ServiceManagerRouter);
+app.use("/suggestion", SuggestManager);
+app.use("/menu", MenuviewsRouter);
+app.use("/check", CheckManagerRouter);
+app.use("/employee", ScheduleRouter);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -42,4 +44,3 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`listening at ${PORT}`);
 });
-
