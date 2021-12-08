491b5e4ecef3eb15b3cbbbb63febd351dd6a4230
ChangSun Park <changsun19991212@gmail.com>
2021-12-09 01:49:59 +0900

    sug hotfix

--- routes/SuggestManager.js
+++ routes/SuggestManager.js
@@ -1,59 +1,68 @@
-const express = require('express');
+const express = require("express");
 const m = require("../models/init-models");
 const sequelize = require("../models").sequelize;
 const models = m(sequelize);
 const Employee_Suggestion = models.Suggestion;
 const router = express.Router();
-router.get('/',async(req,res,next)=>{
-    try{
-        const suggestions = await Employee_Suggestion.findAll(
-            {include :{model: models.Employee, as: "employee",attributes:['name']}}
-        );
-        res.send({suggestions});
-    }catch(error){
-        next(error);
-    }
+router.get("/", async (req, res, next) => {
+  try {
+    const suggestions = await Employee_Suggestion.findAll(
+      // {include :{model: models.Employee, as: "employee",attributes:['name']}}
+      {
+        include: {
+          model: models.Employee,
+          as: "employee",
+          attributes: ["name"],
+        },
+      }
+    );
+    res.send({ suggestions });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:suggestionID',async(req,res,next)=>{
-    const {suggestionID} = req.params;
-    try{
-        const suggestions = await Employee_Suggestion.findAll({
-            include :{model: models.Employee, as: "employee",attributes:['name']},
-            where : {suggestion_id : suggestionID}
-        });
-        res.send({suggestions});
-    }catch(error){
-        next(error);
-    }
+router.get("/:suggestionID", async (req, res, next) => {
+  const { suggestionID } = req.params;
+  try {
+    const suggestions = await Employee_Suggestion.findAll({
+      include: { model: models.Employee, as: "employee", attributes: ["name"] },
+      where: { suggestion_id: suggestionID },
+    });
+    res.send({ suggestions });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.patch('/:suggestionID',async(req,res,next)=>{
-    const {suggestionID} = req.params;
-    const {result} = req.body;
-    const {is_approved} = req.body;
-    try{
-        await Employee_Suggestion.update({
-            result, is_approved
-        },{where : {suggestion_id : suggestionID}});
-        const updateresult =  await Employee_Suggestion.findByPk(suggestionID);
-        res.json(updateresult);
-    }catch(error){
-        next(error);
-    }
+router.patch("/:suggestionID", async (req, res, next) => {
+  const { suggestionID } = req.params;
+  const { result } = req.body;
+  const { is_approved } = req.body;
+  try {
+    await Employee_Suggestion.update(
+      {
+        result,
+        is_approved,
+      },
+      { where: { suggestion_id: suggestionID } }
+    );
+    const updateresult = await Employee_Suggestion.findByPk(suggestionID);
+    res.json(updateresult);
+  } catch (error) {
+    next(error);
+  }
 });
 
-
-router.post('/', function(req,res){
-    const {title,content,result} = req.body;
-    Employee_Suggestion.create({
-        content : content,
-        title :  title,
-        result : result
-    }).then(function(createSuggestion){
-        res.json({title,content,result});
-    })
+router.post("/", function (req, res) {
+  const { title, content, result } = req.body;
+  Employee_Suggestion.create({
+    content: content,
+    title: title,
+    result: result,
+  }).then(function (createSuggestion) {
+    res.json({ title, content, result });
+  });
 });
-
 
 module.exports = router;
